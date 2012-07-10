6118dfee0cd658edeb547df02a562b289f9fd5be
Brett Memsic <memsic@gmail.com>
2012-07-09 17:54:57 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeesApp::Application.config.secret_token = '7f8603c42eb47b1ce684154460754533016504b6b1e5866792564acaae6494c1bfa5e900cfd62b9b06a9b2eaa7f07a3a5b73e9772009a5658b95a0f303c5ba84'
